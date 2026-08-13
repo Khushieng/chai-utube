9161a45e37b3879ace6b5e13f8dbc36f9f9a0f41
Khushieng <engkhushi5@gmail.com>
2026-08-13 13:41:56 +0000

    container config data

---
+++ 01_basics/01_var.js
@@ -0,0 +1,21 @@
+const accountId =8743
+let accountEmail = "his@google.com"
+var accountPassword = "879"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2//
+
+accountemail = "hi@HashChangeEvent.com"
+accountPassword ="32"
+accountCity = "banglaur"
+
+console.log(accountId);
+
+/*
+var ka use n kre bcz of issue of block scope and 
+functional scope
+
+
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict";//treat all Js code as newer version//
+
+//alert(5+5) we are using node Js not browser
+
+console.log("hitesh");
+
+
+let name = "hitesh"
+let age = "46"
+let isLoggedIn = false
+let state;
+
+// number =>2^53
+//bignit
+//string =>""
+//boolean =>true/false
+//null =>standalone vlue
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
