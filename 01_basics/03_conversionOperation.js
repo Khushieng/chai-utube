a0654a8d9753a6cd3ede18f09761c24615893da8
Khushieng <engkhushi5@gmail.com>
2026-08-13 14:30:56 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"90" =>90
+//"true => 1; false=0"
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=>true;
+//"hii" = true;
+//""= false
+
+
+let someNumber = 5
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
