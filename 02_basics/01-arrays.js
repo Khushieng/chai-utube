81a0f941314da53a1f5e3eb47352548b8972ca67
Khushieng <engkhushi5@gmail.com>
2026-08-16 17:23:22 +0000

    01-arrays.js

---
+++ 02_basics/01-arrays.js
@@ -0,0 +1,12 @@
+//   ARRAYS
+
+const myArr = [0, 32, 3, 4, 5, ]
+const myHeros = ["lapuu", "jhapii", "pappu", "tattu"]
+const myArray2 = new Array(1, 2, 3, 5)
+console.log(myArray2);
+
+// Array Method
+
+myArr.push(6)
+//console.log(myArr);
+
