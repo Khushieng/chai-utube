39877d86e5c4e29211b2928dd3674a4d08eecb06
Khushieng <engkhushi5@gmail.com>
2026-08-16 14:44:58 +0000

    container config data

--- 01_basics/05-strings.js
+++ 01_basics/05-strings.js
@@ -1,12 +1,29 @@
 const name = "hitesh"     
 const repoCount = 56
-console.log( `hello my is ${name}and my repo count is $
-    {repoCount}`);
+//console.log( `hello my is ${name}and my repo count is $
+   // {repoCount}`);
 
-const gameName = String('htsanu')
-console.log(gameName[4]);
-console.log(gameName.__proto__);
+const gameName = String('htsan=u=v')
+//console.log(gameName[4]);
+//console.log(gameName.__proto__);
 
-console.log (gameName.length);
-console.log (gameName.toUppercase());
-console.log (gameName .charAt(3));
+//console.log (gameName.length);
+//console.log (gameName.toUpperCase());
+//console.log (gameName .charAt(3));
+//console.log (gameName.indexOf('a'));
+
+const newString = gameName.substring(0, 4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-4, 7)
+console.log(anotherString);
+
+const newStringOne = "    htisg    " 
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com%20chaudhry"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('hh'));
+console.log(gameName.split('='));
