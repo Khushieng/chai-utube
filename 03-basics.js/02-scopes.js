1e5704938b2ee70b1c6c3a7a98d68865b3e404f9
Khushieng <engkhushi5@gmail.com>
2026-08-21 17:22:08 +0000

    config container datatype

---
+++ 03-basics.js/02-scopes.js
@@ -0,0 +1,49 @@
+// var = 3
+let b =45
+if (true){
+    const a =12
+    let b = 8
+    var c = 78
+   // console.log("inner:", b)
+}
+
+//console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+
+    const username ="sonu"
+
+    function two(){
+        const website = "youtube"
+      //  console.log(username);
+    }
+    two()
+}
+//one()
+
+if (true){
+    const username = "golu"
+    if (username == "golu"){
+        const website =" youtube"
+        //console.log(username + website)
+    }
+
+}
+
+
+
+//******** intersting */
+
+function addone(num){
+    return num +1
+}
+
+addone(5)
+
+const addtwo = function(num){
+    return num +3 
+}
+
+addtwo(3)
