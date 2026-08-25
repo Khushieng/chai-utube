9525a7966e403231be98610784c9c69ae392e3bd
Khushieng <engkhushi5@gmail.com>
2026-08-25 10:23:27 +0000

    container config datatype

---
+++ 05-iterations/five.js
@@ -0,0 +1,38 @@
+const coding = ["js", "rubj", "das", "gux"]
+
+// coding.forEach ( function name (item) {
+// //     console.log(item);
+// // } )
+
+// coding.forEach ((item) => {
+//     console.log(item);
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+
+// })
+
+const myCoding = [
+    {
+       languageName: "javaScript",
+       languageFileName: "js"               
+    },
+    {
+       languageName: "java",
+       languageFileName: "java"               
+    },
+    {
+       languageName: "python",
+       languageFileName: "p"               
+    }
+]
+ myCoding.forEach ((index) => {
+    console.log(index.languageFileName);
+    
+})
---
+++ 05-iterations/six.js
@@ -0,0 +1,9 @@
+// const coding = ["js", "rubj", "das", "gux"]
+
+//  const value = coding.forEach ((index) => {
+//     // console.log(index);
+//     return index
+// })
+// console.log(value);
+
+const mynums = [1, 2, 3, 4, 5, 5, 6, 7, ]
